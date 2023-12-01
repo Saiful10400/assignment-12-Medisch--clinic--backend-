be4fca294cd73750b184266d0d993f99cc0ae0a4
saiful10400 <saifulislam10400i5@gmail.com>
2023-12-02 02:40:08 +0600

    uer order api done.

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://192.168.1.4:5173","https://6565dd1cad3d816d1934d906--effervescent-sorbet-8fa644.netlify.app"],
+    origin: [
+      "http://localhost:5173",
+      "http://192.168.1.4:5173",
+      "https://65669cdc2560210076ee3770--friendly-duckanoo-e84426.netlify.app",
+      "https://6565dd1cad3d816d1934d906--effervescent-sorbet-8fa644.netlify.app",
+    ],
     credentials: true,
   })
 );
@@ -41,6 +46,21 @@ async function run() {
     });
     //
 
+    // jwt token verification.
+
+    const verify = (req, res, next) => {
+      const token = req.cookies.token;
+      if (!token) {
+        res.send([]);
+        return;
+      } else if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+          req.tokenEmail = decode.email;
+          next();
+        });
+      }
+    };
+
     // stripe payment.
 
     app.post("/create-payment-intent", async (req, res) => {
@@ -68,9 +88,12 @@ async function run() {
       const result = await bannerDb.insertOne(data);
       res.send(result);
     });
-    app.get("/get_banners", async (req, res) => {
-      const result = await bannerDb.find().toArray();
-      res.send(result);
+    app.get("/get_banners", verify, async (req, res) => {
+      const email = req.query.email;
+      if (email === req.tokenEmail) {
+        const result = await bannerDb.find().toArray();
+        res.send(result);
+      }
     });
     app.patch("/banner_update", async (req, res) => {
       const { bannerId } = req.body;
@@ -111,8 +134,20 @@ async function run() {
       const result = await userDb.insertOne(data);
       res.send(result);
     });
-    app.get("/get_users", async (req, res) => {
-      const result = await userDb.find().sort({ _id: -1 }).toArray();
+    app.get("/get_users", verify, async (req, res) => {
+      const email = req.query.email;
+
+      if (email === req.tokenEmail) {
+        const result = await userDb.find().sort({ _id: -1 }).toArray();
+
+        res.send(result);
+      }
+    });
+    app.get("/get_users_admin", async (req, res) => {
+     
+
+      const result = await userDb.find().toArray();
+
       res.send(result);
     });
     app.patch("/update_user", async (req, res) => {
@@ -138,39 +173,35 @@ async function run() {
       res.send(result);
     });
 
-
-    app.post("/single_userdata",async(req,res)=>{
-      // const data=req.body.email
+    app.post("/single_userdata", verify, async (req, res) => {
+      const data = req.body.email;
       // const query={email:data}
-      const result =await userDb.find().toArray()
-      res.send(result)
-    })
-    app.patch("/single_userUpdate",async(req,res)=>{
-      const data=req.body
-      const updatedoc={
-        $set:{
-          name:data.name,
-          bloodGroup:data.bloodGorup,
-          upazila:data.upazila,
-          district:data.district
-
-        }
+      if (data === req.tokenEmail) {
+        const result = await userDb.find().toArray();
+        res.send(result);
       }
-     const query={_id:new ObjectId(data.id)}
-     const result= await userDb.updateOne(query,updatedoc)
-     res.send(result)
+    });
+    app.patch("/single_userUpdate", async (req, res) => {
+      const data = req.body;
+      const updatedoc = {
+        $set: {
+          name: data.name,
+          bloodGroup: data.bloodGorup,
+          upazila: data.upazila,
+          district: data.district,
+        },
+      };
+      const query = { _id: new ObjectId(data.id) };
+      const result = await userDb.updateOne(query, updatedoc);
+      res.send(result);
+    });
 
-    })
-    
-   
-//  getting banner data.
-app.get("/get_banner_data",async(req,res)=>{
-  const recomandDb=dbName.collection("recomendation")
-  const result=await recomandDb.find().toArray()
-  res.send(result)
-})
-
-
+    //  getting banner data.
+    app.get("/get_banner_data", async (req, res) => {
+      const recomandDb = dbName.collection("recomendation");
+      const result = await recomandDb.find().toArray();
+      res.send(result);
+    });
 
     // handle test.
 
@@ -215,34 +246,56 @@ app.get("/get_banner_data",async(req,res)=>{
       const result = await testDb.findOne(query);
       res.send(result);
     });
-    app.post("/decrement_item_slots",async(req,res)=>{
-      const id=req.body.id
-      const query={_id:new ObjectId(id)}
-      const modify={
-        $inc:{slots:-1,reservation:1}
-      }
-      const result=await testDb.updateOne(query,modify)
-      res.send(result)
-    })
-
+    app.post("/decrement_item_slots", async (req, res) => {
+      const id = req.body.id;
+      const query = { _id: new ObjectId(id) };
+      const modify = {
+        $inc: { slots: -1, reservation: 1 },
+      };
+      const result = await testDb.updateOne(query, modify);
+      res.send(result);
+    });
 
     // handle booked order.
-const bookedDb=dbName.collection("Booked Service")
-    app.post("/add_booked_item",async(req,res)=>{
-      const data=req.body
-      const result=await bookedDb.insertOne(data)
-      res.send(result)
-
-    })
-    app.get("/booked_data",async(req,res)=>{
-      const email=req.query.email
-      const query={userEmail:email}
-      const result=await bookedDb.find(query).sort({_id:-1}).toArray()
-      res.send(result)
-    })
-    app.post("/delete_item",async(req,res)=>{
-      const id=req.body.id
-      const query={_id:new ObjectId(id)}
+    const bookedDb = dbName.collection("Booked Service");
+    app.post("/add_booked_item", async (req, res) => {
+      const data = req.body;
+      const result = await bookedDb.insertOne(data);
+      res.send(result);
+    });
+    app.get("/booked_data", async (req, res) => {
+      const email = req.query.email;
+      const query = { userEmail: email };
+      const result = await bookedDb.find(query).sort({ _id: -1 }).toArray();
+      res.send(result);
+    });
+    app.post("/delete_item", async (req, res) => {
+      const id = req.body.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookedDb.deleteOne(query);
+      res.send(result);
+    });
+    app.get("/get_all_bookedData", async (req, res) => {
+      const result = await bookedDb.find().toArray();
+      res.send(result);
+    });
+    app.post("/update_cv_link", async (req, res) => {
+      const link = req.body.link;
+      const id = req.body.id;
+      const query = { _id: new ObjectId(id) };
+      const updateddoc = {
+        $set: {
+          report: link,
+        },
+      };
+      // const result=await bookedDb.updateOne(query,updateddoc)
+      bookedDb
+        .updateOne(query, updateddoc)
+        .then((response) => res.send(response));
+    });
+    app.post("/delete_booked_test",async(req,res)=>{
+      const data=req.body.id
+      const query={_id:new ObjectId(data)}
       const result=await bookedDb.deleteOne(query)
       res.send(result)
     })
